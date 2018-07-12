a71fe04a8c538dc56b8b586564c9a2c41ae36fd0
hanabi1224 <hailmu@microsoft.com>
2018-07-12 14:44:18 -0700

    fix

--- codecov.bat
+++ codecov.bat
@@ -1,4 +1,4 @@
 choco install opencover.portable
 choco install codecov
-OpenCover.Console.exe -register:user -target:"C:\Program Files\dotnet\dotnet.exe" -targetargs:"test --framework net472 --verbosity q"
+OpenCover.Console.exe -register:user -target:"C:\Program Files\dotnet\dotnet.exe" -targetargs:"test --framework net452 --verbosity q"
 codecov -f "results.xml" 
