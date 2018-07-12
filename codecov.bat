469e05de68540e20d803b9bc643148c1f898155e
hanabi1224 <hailmu@microsoft.com>
2018-07-12 15:19:38 -0700

    Code coverage

--- codecov.bat
+++ codecov.bat
@@ -1,4 +1,5 @@
 choco install opencover.portable
 choco install codecov
+set IsInCodeCoverageTask=1
 OpenCover.Console.exe -register:user -target:"C:\Program Files\dotnet\dotnet.exe" -targetargs:"test -c Release --verbosity q"
 codecov -f "results.xml" 
